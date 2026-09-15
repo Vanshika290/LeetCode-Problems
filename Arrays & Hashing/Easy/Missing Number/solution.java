3fb6edbc0fa708cec6b137a8480e1cd810fbee08
Vanshika Saxena <145781723+Vanshika290@users.noreply.github.com>
2026-09-15 13:35:58 +0530

    Solved: Missing Number [Easy]

---
+++ Arrays & Hashing/Easy/Missing Number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        
+        return expectedSum - sum;
+    }
+}
